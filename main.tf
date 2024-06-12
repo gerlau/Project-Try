60b7a67a18b25f0eab33fa7085c8d88aa6e49fd3
gerlau <gerlaujle@gmail.com>
2024-06-12 22:08:34 +0800

    resolving indentation issues

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "proj-try-terraform-state-dev"
-    key = "terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
